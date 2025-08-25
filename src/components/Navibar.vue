8be97e7608e4329312e9aed52d15ae0e93dd4dae
CoozillaX <79683806+CoozillaX@users.noreply.github.com>
2025-08-25 21:42:57 +1000

    add navibar component

---
+++ src/components/Navibar.vue
@@ -0,0 +1,37 @@
+<template>
+  <nav class="navbar navbar-expand-sm navbar-light bg-light border-bottom">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">
+        <img src="/src/assets/imgs/logo.svg" alt="Nutrition NFP" height="40" />
+      </a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav ms-auto gap-2">
+          <li class="nav-item"><a class="nav-link" href="#">Courses</a></li>
+          <li class="nav-item"><a class="nav-link" href="#">Recipes</a></li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Meal Planner</a>
+          </li>
+          <li class="nav-item">
+            <a class="btn btn-outline-dark btn-sm mt-1 w-100" href="#"
+              >Sign in</a
+            >
+          </li>
+          <li class="nav-item">
+            <a class="btn btn-dark btn-sm mt-1 w-100" href="#">Register</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
