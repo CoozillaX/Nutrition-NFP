a827a9e10c921a74deaf17592dfaa79e0fe29220
CoozillaX <79683806+CoozillaX@users.noreply.github.com>
2025-08-25 21:43:18 +1000

    add footer component

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,23 @@
-<script setup></script>
+<script setup>
+import Navibar from "./components/Navibar.vue";
+import Footer from "./components/Footer.vue";
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <div class="d-flex flex-column min-vh-100">
+    <Navibar></Navibar>
+    <main class="flex-grow-1">
+      <h1>You did it!</h1>
+      <p>
+        Visit
+        <a href="https://vuejs.org/" target="_blank" rel="noopener"
+          >vuejs.org</a
+        >
+        to read the documentation
+      </p>
+    </main>
+    <Footer></Footer>
+  </div>
 </template>
 
 <style scoped></style>
---
+++ src/components/Footer.vue
@@ -0,0 +1,46 @@
+<template>
+  <footer class="border-top py-4 bg-white">
+    <div class="container-fluid d-flex justify-content-between flex-wrap">
+      <div>
+        <div class="fw-bold mb-2">
+          <img
+            src="/src/assets/imgs/logo-dark.svg"
+            alt="Nutrition NFP"
+            height="42"
+          />
+        </div>
+        <div class="d-flex gap-3">
+          <i class="bi bi-twitter"></i>
+          <i class="bi bi-instagram"></i>
+          <i class="bi bi-youtube"></i>
+          <i class="bi bi-linkedin"></i>
+        </div>
+      </div>
+      <div class="row w-75">
+        <div class="col-md-4">
+          <h6>About</h6>
+          <ul class="list-unstyled small">
+            <li><a href="#">About Us</a></li>
+            <li><a href="#">Mission & Vision</a></li>
+            <li><a href="#">Partners</a></li>
+            <li><a href="#">Privacy Policy</a></li>
+          </ul>
+        </div>
+        <div class="col-md-4">
+          <h6>Support</h6>
+          <ul class="list-unstyled small">
+            <li><a href="#">FAQs</a></li>
+            <li><a href="#">Accessibility Statement</a></li>
+          </ul>
+        </div>
+        <div class="col-md-4">
+          <h6>Contact</h6>
+          <ul class="list-unstyled small">
+            <li>Email: info@nutritionnfp.org</li>
+            <li>Phone: +61 3 1234 5678</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </footer>
+</template>
